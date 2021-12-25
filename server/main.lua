d5f60b54076c257090ca9641d8d5a967e7a75f61
FFM-Developpement <96653877+FFM-Developpement@users.noreply.github.com>
2021-12-25 14:38:01 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,11 @@
+-- Chargement de la langue
+local langue = Langues[Config.langue]
 
+-- Fonction MeCommande
+local function MeCommande(source, args)
+    local text = "* " .. langue.prefixe .. table.concat(args, " ") .. " *"
+    TriggerClientEvent('ffm_me:meSync', -1, text, source)
+end
+
+-- Enregistrement de la commande
+RegisterCommand(langue.commandeNom, MeCommande)
