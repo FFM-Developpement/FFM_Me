6c7c2522ae65f5218ffe632b1a7580b7baaada67
FFM-Developpement <96653877+FFM-Developpement@users.noreply.github.com>
2021-12-25 13:47:17 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kreams#0885'
+description 'FFM-Me'
+version '1.0'
+
+
+shared_script 'config.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
